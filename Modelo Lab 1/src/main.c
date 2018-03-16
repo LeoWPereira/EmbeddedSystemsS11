3891cb23f157e7a03bb44ba0e8ea6ba23ce50588
Leonardo Pereira <leonardo.pereira@intranet.agres>
2018-03-16 19:34:59 -0300

    Adiciona inicio do programa

--- Modelo Lab 1/src/main.c
+++ Modelo Lab 1/src/main.c
@@ -35,7 +35,25 @@
 
 /* Private typedef -----------------------------------------------------------*/
 
+typedef struct {
+     uint8_t fileType;
+       
+     uint16_t width;
+     uint16_t height;
+     
+     uint16_t maxGrayValue;
+     
+     uint8_t *data;
+} PGMImage;
+
 /* Private define ------------------------------------------------------------*/
+
+#define P2                    2
+#define P5                    5
+
+#define PGM_FILENAME          "teste24x7.pgm"
+
+#define ERROR_FOPEN           "File could not be opened"
 
 /* Private macro -------------------------------------------------------------*/
 
@@ -43,14 +61,75 @@
 
 /* Private Function Prototypes -----------------------------------------------*/
 
+void informError(uint8_t* str);
+
+uint8_t readPGM(FILE *fp,
+                PGMImage *stPGM);
+
 /* Private Functions Declaration ---------------------------------------------*/
 
 int main()
 {
-   int i;  
+   FILE *myFile;
    
-   i = meanfilter3(4, 9, 0, 0);
+   PGMImage *pgmImage;
+   
+   myFile = fopen(PGM_FILENAME, 
+                  "r");
+    
+   if(!myFile)
+   {
+      informError(ERROR_FOPEN);
+     
+      return 1;
+   }
+   
+   readPGM(myFile,
+           pgmImage);
+   
+   /*int i;  
+   
+   i = meanfilter3(4, 9, 0, 0);*/
    
    return 0;
 }
 
+/**
+ * @brief
+ */
+void informError(uint8_t* str)
+{
+   printf((char *)str);
+  
+   return;
+}
+
+uint8_t readPGM(FILE *fp,
+                PGMImage *stPGM)
+{
+   char buffer[128];
+   
+   //read image format
+   if (!fgets(buffer, sizeof(buffer), fp)) 
+   {
+      return 1;
+   }
+  
+   //check the image format
+   if(buffer[0] != 'P')
+   { 
+      return 1;
+   }
+   
+   if(buffer[1] == '2')
+   {
+      stPGM->fileType = P2;
+   }
+   
+   else if(buffer[1] == '5')
+   {
+      stPGM->fileType = P5;
+   }
+   
+   return 0;
+}
