7f261d933a79a0d8a6186440a30c2baf666a247c
LeoWPereira <leonardowinterpereira@gmail.com>
2018-03-22 21:07:37 -0300

    atualiza main

--- Modelo Lab 1/src/asm.s
+++ Modelo Lab 1/src/asm.s
@@ -23,7 +23,52 @@
  * @retval  R0 : total pixels written on img_out[]
  */
 meanfilter3:
-   //PUSH 
+   PUSH         {R0, R3-R11, LR}
+   SUB          SP, SP, #0x8
+   MOV          R0, R1 // porque?
+   
+   //
+   // uint32_t totalPixelsImagemSaida = 0
+   //
+   MOVS         R1, #0 // porque?
+   
+   //
+   //uint16_t dim_x_imagem_saida = (dim_x - MASK_SIZE + 1);
+   //
+   
+   
+   //
+   //uint16_t dim_y_imagem_saida = (dim_y - MASK_SIZE + 1);
+   //
+   
+   //
+   //uint32_t posicaoAtualEntrada = 0;
+   //
+   
+   //
+   //uint32_t posicaoAtualSaida = 0;
+   //
+   
+   //
+   //uint16_t linha1;
+   //
+   
+   //
+   //uint16_t linha2;
+   //
+   
+   //
+   //uint16_t linha3;
+   //
+   
+   //
+   //uint16_t colunas = 0;
+   //
+   
+   //
+   //uint16_t linhas = 0;
+   //
+   
    
    bx lr
 
