ee6a552b342d3ce04e6f7348706eb970461258fd
LeoWPereira <leonardowinterpereira@gmail.com>
2018-03-29 20:48:19 -0300

    ganha mais 3%

--- Modelo Lab 1/src/asm.s
+++ Modelo Lab 1/src/asm.s
@@ -26,18 +26,14 @@ meanfilter3:
    PUSH         {R0, R3-R11, LR}
    SUB          SP, SP, #0x8
 
-   //
-   // uint16_t dim_x_imagem_saida = (dim_x - MASK_SIZE + 1)
-   //
+   // R3[0..1] vai armazenar dim_x_imagem_saida (dim_x - MASK_SIZE + 1)
+   // R3[3..2] vai armazenar dim_y_imagem_saida (dim_y - MASK_SIZE + 1)
    SUBS         R3, R0, #0x2 // 0x2 = MASK_SIZE - 1
-   STR          R3, [SP]
-
-   //
-   // uint16_t dim_y_imagem_saida = (dim_y - MASK_SIZE + 1)
-   //
-   SUBS         R3, R1, #0x2
+   SUBS         R10, R1, #0x2
+   ADD          R3, R3, R10, LSL #0x10
    
    MOV          R0, R1
+   
    //
    // uint32_t totalPixelsImagemSaida = 0
    //
@@ -57,13 +53,17 @@ meanfilter3:
    // uint16_t linhas = 0
    //
    MOV          R5, R0
-   LDR          R6, [SP]
    
+   // compara dim_x_imagem_saida
+   UBFX         R6, R3, #0x0, #0x10
    CMP          R6, #0x1
    BLT          ACABA_FILTRO
    
+   MOV          R10, #0x1
+   RSB          R4, R10, R3, LSR #0x10
+   
    LDRH         R10, [SP, #0x8]
-   SUBS         R4, R3, #0x1
+   
    STR          R4, [SP, #0x4]
    
   //
@@ -84,7 +84,6 @@ LOOP_COLUNAS_IMAGEM_SAIDA:
     //
     // posicaoAtualEntrada += dim_x
     //
-    //MOV         LR, R10
     
     //
     // linha2 = img_in[posicaoAtualEntrada] + img_in[posicaoAtualEntrada + 1] + img_in[posicaoAtualEntrada + 2]
@@ -97,10 +96,13 @@ LOOP_COLUNAS_IMAGEM_SAIDA:
     ADD         R9, R9, R7
     ADD         R4, R4, R9
     
-    CMP         R3, #0x1
+    // Compara dim_y_imagem_saida com 0x1
+    UBFX        R7, R3, #0x10, #0x10
+    CMP         R7, #0x1
     BLT         FIM_LOOP_LINHAS_IMAGEM_SAIDA
-    
-    CMP         R5, R3
+
+    // Compara linhas (R5) com dim_y_imagem_saida
+    CMP         R5, R3, LSR #0x10
     BGE         FIM_LOOP_LINHAS_IMAGEM_SAIDA
     
     //
@@ -153,7 +155,9 @@ LOOP_LINHAS_IMAGEM_SAIDA:
     //
     // posicaoAtualSaida += dim_x_imagem_saida
     //
-    LDR         R7, [SP]
+    // carrega dim_x_imagem_saida em R7
+    UBFX         R7, R3, #0x0, #0x10
+    
     ADD         R8, R8, R7
     
     //
@@ -169,7 +173,8 @@ LOOP_LINHAS_IMAGEM_SAIDA:
 FIM_LOOP_COLUNAS_IMAGEM_SAIDA:
     ADDS        R5, R5, #0x1
     
-    CMP         R5, R3
+    // compara dim_y_imagem_saida
+    CMP         R5, R3, LSR #0x10
     BLT         LOOP_LINHAS_IMAGEM_SAIDA
     
 FIM_LOOP_LINHAS_IMAGEM_SAIDA:
@@ -191,7 +196,8 @@ FIM_LOOP_LINHAS_IMAGEM_SAIDA:
     MOV         R10, R0
     ADDS        R6, R6, #0x1
     
-    LDR         R4, [SP]
+    // carrega dim_x_imagem_saida em R4
+    UBFX        R4, R3, #0x0, #0x10
     
     CMP         R8, R4
     BLT         LOOP_COLUNAS_IMAGEM_SAIDA
