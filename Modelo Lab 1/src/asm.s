b9e3f8df3d70427ee0c9e8fc86110403963b6ff9
Leonardo Pereira <leonardo.pereira@intranet.agres>
2018-04-02 13:38:45 -0300

    Arruma organizacao do codigo

--- Modelo Lab 1/src/asm.s
+++ Modelo Lab 1/src/asm.s
@@ -34,24 +34,16 @@ meanfilter3:
    
    MOV          R0, R1
    
-   //
    // uint32_t totalPixelsImagemSaida = 0
-   //
    MOVS         R1, #0
    
-   //
    // uint32_t posicaoAtualSaida = 0
-   //
    MOV          R8, R1
    
-   //
    // uint16_t colunas = 0
-   //
    MOVS         R0, #0x0
    
-   //
    // uint16_t linhas = 0
-   //
    MOV          R5, R0
    
    // compara dim_x_imagem_saida
@@ -68,14 +60,10 @@ meanfilter3:
    LDRH        R6, [SP, #0x8]
    MOV         R10, R6
    
-  //
   // for(; colunas < dim_x_imagem_saida; colunas++)
-  //
 LOOP_COLUNAS_IMAGEM_SAIDA:
 
-    //
     // linha1 = img_in[posicaoAtualEntrada] + img_in[posicaoAtualEntrada + 1] + img_in[posicaoAtualEntrada + 2] 
-    //
     LDR         R12, [R2]
     UBFX        R4, R12, #0x8, #0x8
     UBFX        R9, R12, #0x10, #0x8
@@ -83,10 +71,7 @@ LOOP_COLUNAS_IMAGEM_SAIDA:
     ADD         R12, R12, R4
     ADD         R12, R12, R9
     
-    //
-    // linha2 = img_in[posicaoAtualEntrada] + img_in[posicaoAtualEntrada + 1] + img_in[posicaoAtualEntrada + 2]
-    //
-    
+    // linha2 = img_in[posicaoAtualEntrada] + img_in[posicaoAtualEntrada + 1] + img_in[posicaoAtualEntrada + 2]    
     // Carrega em R9 a posicao inicial da linha1 + dim_x
     LDR         R9, [R10, R2]
     UBFX        R7, R9, #0x8, #0x8
@@ -104,20 +89,13 @@ LOOP_COLUNAS_IMAGEM_SAIDA:
     CMP         R5, R3, LSR #0x10
     BGE         FIM_LOOP_LINHAS_IMAGEM_SAIDA
     
-    //
     // for(; linhas < dim_y_imagem_saida; linhas++)
-    // 
 LOOP_LINHAS_IMAGEM_SAIDA:
     
-    //
     // posicaoAtualEntrada += dim_x
-    //
     ADD         R10, R10, R6
     
-    //
-    // linha3 = img_in[posicaoAtualEntrada] + img_in[posicaoAtualEntrada + 1] + img_in[posicaoAtualEntrada + 2]
-    //
-    
+    // linha3 = img_in[posicaoAtualEntrada] + img_in[posicaoAtualEntrada + 1] + img_in[posicaoAtualEntrada + 2]    
     LDR         R7, [R10, R2]
     UBFX        R11, R7, #0x8, #0x8
     UBFX        R9, R7, #0x10, #0x8
@@ -125,49 +103,34 @@ LOOP_LINHAS_IMAGEM_SAIDA:
     ADD         R7, R7, R11
     ADD         R9, R9, R7
     
-    //
     // img_out[posicaoAtualSaida] = (((linha1 + linha2 + linha3) * (uint32_t)0xE38F) >> 17) >> 2
-    //
     ADD         R7, R4, R12
     ADD         R7, R7, R9
+    
     // Realiza divisao a partir de multiplicacao e shift
-    //MOV         R11, #0x9
-    //SDIV        R7, R7, R11  ; Signed divide, R7 = R7/R11
     MOV         R11, #0xE38F
     MUL         R7, R7, R11
     LSRS        R7, R7, #0x13
     LDR         R11, [SP, #0xC]
     STRB        R7, [R8, R11]
     
-    //
     // totalPixelsImagemSaida++
-    //
     ADDS        R1, R1, #0x1
     
-    //
-    // if(linhas < (dim_y_imagem_saida - 1))
-    //
-    LDR         R7, [SP, #0x4]
-    
-    CMP         R5, R7
+    // if(linhas < (dim_y_imagem_saida - 1)) 
+    CMP         R5, R3, LSR #0x10
     BGE         FIM_LOOP_COLUNAS_IMAGEM_SAIDA
     
-    //
     // posicaoAtualSaida += dim_x_imagem_saida
-    //
     // carrega dim_x_imagem_saida em R7
     UBFX         R7, R3, #0x0, #0x10
     
     ADD         R8, R8, R7
     
-    //
     // linha1 = linha2
-    //
     MOV         R12, R4
     
-    //
     // linha2 = linha3
-    //
     MOV         R4, R9
     
 FIM_LOOP_COLUNAS_IMAGEM_SAIDA:
@@ -179,31 +142,23 @@ FIM_LOOP_COLUNAS_IMAGEM_SAIDA:
     
 FIM_LOOP_LINHAS_IMAGEM_SAIDA:
     
-    //
     // Adiciona #0x1 na posicao inicial de leitura R2
-    //
     ADD          R2, R2, #0x1
     
-    //
     // linhas = 0
-    //
     MOVS         R5, #0x0
     
-    //
     // posicaoAtualSaida = (colunas + 1)
-    //
     ADD         R8, R0, #0x1
     
-    //
     // verifica loop colunas
-    //
     MOV         R0, R8
     MOV         R10, R6
     
     // carrega dim_x_imagem_saida em R4
     UBFX        R4, R3, #0x0, #0x10
     
-    CMP         R8, R4
+    CMP         R0, R4
     BLT         LOOP_COLUNAS_IMAGEM_SAIDA
    
 ACABA_FILTRO:
