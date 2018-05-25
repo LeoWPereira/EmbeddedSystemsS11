e621603c6449b2a64ce6c26c337cfd4e8341bda9
LeoWPereira <leonardowinterpereira@gmail.com>
2018-05-24 21:30:15 -0300

    adiciona arquivos no projeto

--- CMSIS_RTOS_IAR8/RoboCC/src/main.c
+++ CMSIS_RTOS_IAR8/RoboCC/src/main.c
@@ -1,5 +1,18 @@
+osThreadId thread_Timer_id;
+osThreadId thread_GUI_id;
+osThreadId thread_Encoder_id;
+osThreadId thread_Control_id;
+osThreadId thread_PWM_id;
 
 int main()
 {
+  osKernelInitialize();
+  SystemInit();
+  
+  // System thread initialization
+  
+  osKernelStart();
+  osDelay(osWaitForever);
+  
   return 0;
 }
---
+++ CMSIS_RTOS_IAR8/RoboCC/src/main.h
@@ -0,0 +1,42 @@
+/**
+ *
+ */
+
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+/****************************************************************************************
+ * DEPENDENCIAS
+ ****************************************************************************************/
+
+#include <stdio.h>
+
+#include "LPC13xx.h" // CMSIS-Core
+#include "cmsis_os.h" // CMSIS-RTOS
+
+#include "mcu_regs.h"
+#include "type.h"
+#include "oled.h"
+#include "gpio.h"
+#include "ssp.h"
+
+/****************************************************************************************
+ * CONSTANTES E MACROS
+ ****************************************************************************************/
+
+#ifndef DEF_TRUE
+#define DEF_TRUE 1
+#endif
+
+#ifndef DEF_FALSE
+#define DEF_FALSE 0
+#endif
+
+#define DISPLAY_FREQUENCY_LINE          30
+#define DISPLAY_LAST_LINE               55
+
+/****************************************************************************************
+ * DEFINICOES DE TIPOS E ESTRUTURAS
+ ****************************************************************************************/
+
+#endif
